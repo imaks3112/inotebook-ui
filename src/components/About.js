50ab23ac63d287c10eacab2526fdc737bf9bfc90
imakshaylankeshwar <akshay.lankeshwar@zylon.io>
2024-06-05 13:04:14 +0530

    Add tab active class in navbar, Implement context and state for the note.

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react';
+import NoteContext from '../context/notes/noteContext';
 
 const About = () => {
+  const a = useContext(NoteContext);
+  useEffect(() => {
+    a.update();
+  }, []) // eslint-disable-line react-hooks/exhaustive-deps
   return (
     <div>
-      This is About
+      This is About {a.state.name} in class {a.state.class}
     </div>
   )
 }
 
-export default About
+export default About;
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      This is Home
+      <h2>Add Note</h2>
     </div>
   )
 }
